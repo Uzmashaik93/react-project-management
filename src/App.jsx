7ac8ce87bb319fa9021462ada9a0b57febaf797f
uzma shaik <uzma97910@gmail.com>
2025-02-12 09:34:30 +0100

    initial components,routes and packages set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <h1>Welcome to Project Management App</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/projects" element={<p>Projectlist</p>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,4 @@
+function HomePage() {
+  return <div>this is home page</div>;
+}
+export default HomePage;
